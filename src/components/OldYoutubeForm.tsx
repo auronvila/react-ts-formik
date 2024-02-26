6946ab8d0798505b2589f0b98b56d4446ac51fb8
auronvila <auronvila1121@gmail.com>
2024-02-26 10:18:00 +0300

    first commit

---
+++ src/components/OldYoutubeForm.tsx
@@ -0,0 +1,58 @@
+import {useFormik} from "formik";
+import * as Yup from 'yup'
+
+interface FormValues {
+  name: string,
+  email: string,
+  channel: string
+}
+
+
+export default function OldYoutubeForm() {
+  const initialValues = {
+    name: '',
+    channel: '',
+    email: ''
+  }
+
+  function handleSubmit(values: FormValues) {
+    console.log(values)
+  }
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required('reqq'),
+    email: Yup.string().email('invalid email').required('req'),
+    channel: Yup.string().required('req')
+  })
+
+  const formik = useFormik<FormValues>({
+    initialValues,
+    validationSchema,
+    onSubmit: handleSubmit
+  });
+
+
+  return (
+    <>
+      <form onSubmit={formik.handleSubmit}>
+        <label htmlFor={'name'}>Name</label>
+        <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.name} id={'name'}
+               name={'name'} type={'text'}/>
+        {formik.touched.name && formik.errors.name && <div style={{color: "red"}}>{formik.errors.name}</div>}
+
+        <label htmlFor={'email'}>E-mail</label>
+        <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email} id={'email'}
+               name={'email'} type={'email'}/>
+        {formik.touched.email && formik.errors.email && <div style={{color: "red"}}>{formik.errors.email}</div>}
+
+        <label htmlFor={'channel'}>Channel</label>
+        <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.channel} id={'channel'}
+               name={'channel'}
+               type={'text'}/>
+        {formik.touched.channel && formik.errors.channel && <div style={{color: "red"}}>{formik.errors.channel}</div>}
+
+        <button type={'submit'}>Submit</button>
+      </form>
+    </>
+  )
+}
---
+++ src/components/YoutubeForm.tsx
@@ -0,0 +1,93 @@
+import {ErrorMessage, Field, FieldArray, Form, Formik, FormikProps} from "formik";
+import * as Yup from 'yup'
+
+interface FormValues {
+  name: string,
+  email: string,
+  channel: string,
+  social: {
+    facebook: string
+  },
+  phoneNumbers: string[]
+}
+
+export default function YoutubeForm() {
+  const initialValues: FormValues = {
+    name: '',
+    channel: '',
+    email: '',
+    social: {
+      facebook: ''
+    },
+    phoneNumbers: ['']
+  }
+
+  function handleSubmit(values: FormValues) {
+    console.log(values)
+  }
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required('Name is required'),
+    email: Yup.string().email('Invalid email format').required('Email is required'),
+    channel: Yup.string().required('Channel is required'),
+    social: Yup.object().shape({
+      facebook: Yup.string().required("Facebook is required")
+    }),
+    phoneNumbers: Yup.array().of(Yup.string())
+  })
+
+  return (
+    <>
+
+      <Formik validationSchema={validationSchema} initialValues={initialValues} onSubmit={handleSubmit}>
+        {(formik: FormikProps<FormValues>) => (
+          <Form>
+            <label htmlFor={'name'}>Name</label>
+            <Field name={'name'} type={'text'}/>
+            <ErrorMessage name={'name'} component="div" className="error-message"/>
+            <label htmlFor={'email'}>E-mail</label>
+            <Field name={'email'} type={'email'}/>
+            <ErrorMessage name={'email'} component="div" className="error-message"/>
+            <label htmlFor={'channel'}>Channel</label>
+            <Field name={'channel'} type={'text'}/>
+            <ErrorMessage name={'channel'} component="div" className="error-message"/>
+            <label htmlFor={'facebook'}>Facebook</label>
+            <Field name={'social.facebook'} type={'text'}/>
+            <ErrorMessage name={'social.facebook'} component="div" className="error-message"/>
+            <div>
+              <label>List of phone numbers</label>
+              <FieldArray name={'phoneNumbers'}>
+                {(arrayHelpers) => (
+                  <div>
+                    {arrayHelpers.form.values.phoneNumbers.map((_phoneNumber: string, index: number) => (
+                      <div style={{display: 'flex', margin: 10}} key={index}>
+                        <Field name={`phoneNumbers.${index}`} type={'text'}/>
+                        <button type={'button'} onClick={() => arrayHelpers.remove(index)}>-</button>
+                        {index === arrayHelpers.form.values.phoneNumbers.length - 1 && arrayHelpers.form.values.phoneNumbers.length < 5 && (
+                          <button type={'button'} onClick={() => arrayHelpers.push('')}>+</button>
+                        )}
+                      </div>
+                    ))}
+                    {arrayHelpers.form.values.phoneNumbers.length === 0 && (
+                      <button type={'button'} onClick={() => arrayHelpers.push('')}>Add Phone Number</button>
+                    )}
+                  </div>
+                )}
+              </FieldArray>
+            </div>
+            <button type={'submit'}>Submit</button>
+            <button type={'button'} onClick={() => formik.setValues({
+              name: 'Auron',
+              email: 'Auron@gmail.com',
+              channel: 'teset',
+              phoneNumbers: [''],
+              social: {facebook: 'Auronvila'}
+            })}>
+              Change field valuesz
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </>
+  )
+}
